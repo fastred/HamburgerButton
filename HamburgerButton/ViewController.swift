1fd3e6265618e3f2a2d15ed8e1367da6b7b66e82
Arkadiusz Holko <fastred@fastred.org>
2014-07-14 15:26:49 +0200

    Initial Commit

---
+++ HamburgerButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HamburgerButton
+//
+//  Created by Arkadiusz on 14-07-14.
+//  Copyright (c) 2014 Arkadiusz Holko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
